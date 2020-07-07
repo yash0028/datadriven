4abefca86f9a0f41b7f09553cc4974e925faaf55
yash yashautomation0028@gmail.com <yash yashautomation0028@gmail.com>
2020-07-07 14:56:54 +0530

    obj

--- Datadriven/src/Class.java
+++ Datadriven/src/Class.java
@@ -2,8 +2,8 @@
 public class Class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Framework : systematic folder creation to manage project.
 	}
 
 }
---
+++ Datadriven/src/com/demowebshop/pageob/Homepage.java
@@ -0,0 +1,8 @@
+package com.demowebshop.pageob;
+
+public class Homepage {
+	
+	//Email , Password , Login Button
+	
+
+}
